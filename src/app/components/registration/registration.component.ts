4811fc5008738f824cd9bf79dfedaa8d031709d8
Nelson <punkpunkada@gmail.com>
2025-06-27 16:34:17 -0300

    add imports in the registration component

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component , OnInit, inject} from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    NgxMaskDirective,
+  ],
+  providers: [
+    provideNgxMask()
+  ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
