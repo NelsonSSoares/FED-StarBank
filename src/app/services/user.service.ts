48b595eed539292d96017e994dc3c0c1c9918235
Nelson <punkpunkada@gmail.com>
2025-06-29 11:59:59 -0300

    fix environments

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,18 +2,21 @@ import { UserRequest } from './../models/userRequest';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  private readonly API_URL = environment.API_URL;
+
   constructor(
     private http: HttpClient,
   ) { }
 
   createUser(user: UserRequest): Observable<UserRequest> {
-    return this.http.post<UserRequest>('http://localhost:8765/starbank/register', user);
+    return this.http.post<UserRequest>(this.API_URL+'/starbank/register', user);
   }
 
   loginUser(user: UserRequest) {
